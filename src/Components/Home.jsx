28a22fe936f008891e5b864101037efac78442e0
Douglas Rodrigues <douglasrodrigues.git@gmail.com>
2024-01-12 22:18:56 -0300

    adicionado as rotas do menu

---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Head from "./Head";
+
+const Home = () => {
+  return (
+    <div>
+      <Head title="Home" description='Home do site Marina Fiori Nail Designer' />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Components/Portfolio.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Head from "./Head";
+
+const Portfolio = () => {
+  return (
+    <div>
+      <Head title='Portfolio' description='Conheça o portfolio de Marina Fiori' />
+      <h1>Portfolio</h1>
+    </div>
+  );
+};
+
+export default Portfolio;
