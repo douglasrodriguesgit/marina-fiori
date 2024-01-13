6fe7af24fb9e86700acf63b3c7c7879b3be26e5d
Douglas Rodrigues <douglasrodrigues.git@gmail.com>
2024-01-13 20:10:41 -0300

    estilizado o menu

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,36 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import Brand from "../Assets/icons/brand.svg?react";
+import Contact from "../Assets/redes/whatsappLight.svg?react";
+import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <NavLink to="/" end>
+    <header className={`${styles.header} "menu"}`}>
+      <NavLink className={styles.brand} to="/" end>
+        {" "}
+        <Brand />
+      </NavLink>
+      <ul className={`${styles.nav} ${styles.hamburguer} `}>
+        <li>
           {" "}
-          <Brand />
-        </NavLink>
-        <NavLink to="/portfolio"> Portfolio</NavLink>
-        <NavLink to="/meconheca"> Me conheça</NavLink>
-        <NavLink to="/preco"> Preço</NavLink>
-        <NavLink to="/depoimentos"> Depoimentos</NavLink>
-      </nav>
+          <NavLink to="/portfolio"> Portfólio</NavLink>{" "}
+        </li>
+        <li>
+          {" "}
+          <NavLink to="/meconheca"> Me conheça</NavLink>{" "}
+        </li>
+        <li>
+          {" "}
+          <NavLink to="/preco"> Preço</NavLink>{" "}
+        </li>
+        <li>
+          <NavLink to="/depoimentos">Depoimentos</NavLink>{" "}
+        </li>
+      </ul>
+      <NavLink className={styles.contact} to="/whatsapp">
+        <Contact />
+      </NavLink>
     </header>
   );
 };
---
+++ src/Components/Whatsapp/Whatsapp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Whatsapp = () => {
+  return (
+    <div>
+      <h1>Whatsapp</h1>
+    </div>
+  );
+};
+
+export default Whatsapp;
