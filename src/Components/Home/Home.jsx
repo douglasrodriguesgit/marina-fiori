d2a5d96cdb7d261fb12387e00d89403fa064a68d
Douglas Rodrigues <douglasrodrigues.git@gmail.com>
2024-01-22 21:20:30 -0300

    estilizado a home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import Head from "../Head";
+import styles from "./Home.module.css";
 
 const Home = () => {
   return (
     <div>
-      <Head title="Home" description='Home do site Marina Fiori Nail Designer' />
+      <Head
+        title="Home"
+        description="Home do site Marina Fiori Nail Designer"
+      />
+      <section className={styles.container}>
+        <div className={styles.info}>
+          <p className="subtitle">by Marina Fiori</p>
+          <h1 className="title">Beleza e saúde das unhas</h1>
+          <p className={`${styles.text} ${"subtitle"}`}>
+            Nail Designer em Campinas
+          </p>
+          <button className={styles.button}>Faça seu agendamento</button>
+        </div>
+        <div className={styles.image}></div>
+      </section>
     </div>
   );
 };
