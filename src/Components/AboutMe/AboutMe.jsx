28133eb1b01fd5fb6b2cd7db45be54bac2214a8b
Douglas Rodrigues <douglasrodrigues.git@gmail.com>
2024-02-06 00:28:34 -0300

    adicionado a sessão about me

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Head from "../Head";
 import TitlePages from "../TitlePages";
+import styles from "./AboutMe.module.css";
 
 const AboutMe = () => {
   return (
@@ -10,6 +11,30 @@ const AboutMe = () => {
         description="Saiba mais sobre a Nail Designer Marina Fiori"
       />
       <TitlePages text="Me conheça" />
+      <section className={styles.container}>
+        <div className={`${"title"} ${styles.brand}`}>
+          <h3>Marina Fiori</h3>
+        </div>
+        <div className={styles.image}></div>
+        <div className={styles.aboutMe}>
+          <p className={`${"subtitle"} ${styles.description}`}>
+            Nail Designer especialista
+          </p>
+          <h2 className={`${"subtitle"} ${styles.title}`}>Sobre mim</h2>
+          <p className={`${"subtitle"} ${styles.text}`}>
+            {" "}
+            Designer de unhas, apaixonada pela solução prática que os
+            alongamentos com fibra de vidro trazem para a vida de toda mulher.
+            Atuo na área há 8 anos, mudando a vida de mulheres.
+          </p>
+          <p className={`${"subtitle"} ${styles.text}`}>
+            {" "}
+            Realizo trabalhos de unhas de fibra de vidro com acabamentos
+            naturais, espessura bem fininha, muito resistente e com a
+            personalização que combine com o seu estilo.
+          </p>
+        </div>
+      </section>
     </div>
   );
 };
